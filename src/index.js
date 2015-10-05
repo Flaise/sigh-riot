c1a7b7de4c2fd43626e484ddd7a7ddf7fd6eb862
Flaise <Flaise.Saffron@gmail.com>
2015-10-05 07:56:57 -0500

    Initial working commit.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 import _ from 'lodash'
-import { Bacon } from 'sigh-core'
-import { mapEvents } from 'sigh-core/lib/stream'
+import {mapEvents} from 'sigh-core/lib/stream'
+
 
 function riotTask(opts) {
-  // this function is called once for each subprocess in order to cache state,
-  // it is not a closure and does not have access to the surrounding state, use
-  // `require` to include any modules you need, for further info see
-  // https://github.com/ohjames/process-pool
-  var log = require('sigh-core').log
-
-  // this task runs inside the subprocess to transform each event
-  return event => {
-    var data, sourceMap
-    // TODO: data = compile(event.data) etc.
-
-    return { data, sourceMap }
-  }
+    const compile = require('riot').compile
+    
+    return event => {
+        const data = `;var riot = require('riot/riot');${compile(event.data, opts)};`
+        
+        return {data, sourceMap: undefined}
+    }
 }
 
 function adaptEvent(compiler) {
-  // data sent to/received from the subprocess has to be serialised/deserialised
   return event => {
     if (event.type !== 'add' && event.type !== 'change')
-      return event
+        return event
 
-    // if (event.fileType !== 'relevantType') return event
+    if (event.fileType !== 'tag')
+        return event
 
-    return compiler(_.pick(event, 'type', 'data', 'path', 'projectPath')).then(result => {
+    return compiler(_.pick(event, 'data')).then(result => {
       event.data = result.data
 
       if (result.sourceMap)
         event.applySourceMap(JSON.parse(result.sourceMap))
 
-      // event.changeFileSuffix('newSuffix')
+      event.changeFileSuffix('js')
       return event
     })
   }
@@ -42,7 +36,7 @@ var pooledProc
 
 export default function(op, opts = {}) {
   if (! pooledProc)
-    pooledProc = op.procPool.prepare(riotTask, opts, { module })
+    pooledProc = op.procPool.prepare(riotTask, opts, {module})
 
   return mapEvents(op.stream, adaptEvent(pooledProc))
 }
